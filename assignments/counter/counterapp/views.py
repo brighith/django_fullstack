1570829c350a0a30d57c6a2689b44ccbdc801e6c
Yousef brighith <128628811+brighith@users.noreply.github.com>
2023-06-07 20:59:45 +0300

    count

--- assignments/counter/counterapp/views.py
+++ assignments/counter/counterapp/views.py
@@ -5,9 +5,6 @@ def index(request):
     if 'counter' in request.session:
         request.session['counter'] += 1
 
-    else:
-        request.session['counter'] = 1
-
     return render(request, 'index.html')
 
 
