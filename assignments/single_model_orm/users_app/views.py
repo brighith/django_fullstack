b701f6d8a711c09faff782c52e65fda241b2a689
Yousef brighith <128628811+brighith@users.noreply.github.com>
2023-06-09 11:28:57 +0300

    users_with_tamplates

--- assignments/single_model_orm/users_app/views.py
+++ assignments/single_model_orm/users_app/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import user
 
 # Create your views here.
+
+
+def index(request):
+    context = {
+        "Users": user.objects.all()
+    }
+
+    return render(request, "index.html", context)
+
+
+def process(request):
+    user.objects.create(first_name=request.POST['first'], last_name=request.POST['last'],
+                        email_address=request.POST['email'], age=request.POST['age'])
+
+    return redirect('/')
