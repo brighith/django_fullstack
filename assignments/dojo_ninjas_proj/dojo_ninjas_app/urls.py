15584a03b02d83956133c70fb8bd53d1f8bcc434
Yousef brighith <128628811+brighith@users.noreply.github.com>
2023-06-10 22:21:34 +0300

    dojo ninja with templates

--- assignments/dojo_ninjas_proj/dojo_ninjas_app/urls.py
+++ assignments/dojo_ninjas_proj/dojo_ninjas_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index)
+    path('', views.index),
+    path('adding_n', views.adding_n),
+    path('adding_d', views.adding_d),
+    path('deleteDojo', views.deleteDojo)
+
 ]
--- assignments/dojo_ninjas_proj/dojo_ninjas_app/views.py
+++ assignments/dojo_ninjas_proj/dojo_ninjas_app/views.py
@@ -1,5 +1,34 @@
-from django.shortcuts import render, HttpResponse
+from django.shortcuts import render, redirect
+from .models import Ninja, Dojo
 
 
 def index(request):
-    return HttpResponse("this is the equivalent of @app.route('/')!")
+    context = {
+        "dojo_objects": Dojo.objects.all(),
+    }
+    return render(request, 'index.html', context)
+
+
+def adding_d(request):
+    Dojo.objects.create(
+        name=request.POST['name'],
+        city=request.POST['city'],
+        state=request.POST['state']
+    )
+    return redirect('/')
+
+
+def adding_n(request):
+    Ninja.objects.create(
+        first_name=request.POST['first'],
+        last_name=request.POST['last'],
+        dojo=Dojo.objects.get(id=request.POST['dojo']),
+    )
+
+    return redirect('/')
+
+
+def deleteDojo(request):
+    dell = Dojo.objects.get(id=request.POST['delete']).delete()
+
+    return redirect('/')
